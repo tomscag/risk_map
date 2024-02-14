6610edfebeba2d2847fafb6697d445ed64ca289c
Tomas Scagliarini <tomscag92@gmail.com>
2024-02-14 16:19:23 +0100

    (Launcher_america_all.py): Fixed small bug

--- Analysis/lib/motter/Launcher_america_all.py
+++ Analysis/lib/motter/Launcher_america_all.py
@@ -29,15 +29,15 @@ def run_cascades(node_to_remove,NN):
 
     # os.system("sed '1 i\#define NN %d' %s > %s" % (NN, script_name_in, script_name_out))
 
-    exe_name = 'aa_%g_%g.out' % (NN, np.random.randint(0, high = 2147483647, size = None))
+    exe_name = './aa_%g_%g.out' % (NN, np.random.randint(0, high = 2147483647, size = None))
     os.system("gcc -g -o %s %s -lm" % (exe_name, script_name_out))
 
 
     randseed = np.random.randint(0, high = 2147483647, size = None)
         
-    fname='input_N_'+str(NN)+'_a_'+str(node_to_remove[0])+'.in'
+    fname='./input_N_'+str(NN)+'_a_'+str(node_to_remove[0])+'.in'
 
-    fout1='Results/N_'+str(NN)+'_a_'+str('{:.6f}'.format(alpha))+'.dat'
+    fout1='Results/N_'+str(NN)+'_a_'+str('{:.3f}'.format(alpha))+'_node_'+str(node_to_remove[0])+'.dat'
 
     finput=open(fname,'w+')
     finput.write('%d ' % NN)
@@ -53,17 +53,17 @@ def run_cascades(node_to_remove,NN):
     os.system('./'+str(exe_name)+' '+fname+' '+str(randseed)) #run with valgrind when updates are made, to check memory is OK
     # os.system('valgrind --leak-check=yes --track-origins=yes ./'+str(exe_name)+' '+fname+' '+str(randseed))
 
-    os.system("rm %s %s" % (exe_name,fname))
+    # os.system("rm %s %s" % (exe_name,fname))
 
 
 
-def run_parallel(NN):
+def run_parallel(NN,numcpu):
     """ Parallelize the execution of the function run_cascades """
-    pool = mp.Pool(3)
+    pool = mp.Pool(numcpu)
     for node_to_remove in range(NN):
         node_to_remove = [node_to_remove] #
-
         pool.apply_async(run_cascades, args = (node_to_remove,NN  ))
+        # run_cascades(node_to_remove,NN)
 
     pool.close()
     pool.join()
@@ -77,6 +77,6 @@ def run_parallel(NN):
 if __name__ == "__main__":
 
     NN = 16167
-    ncores = 8
+    numcpu = 20
 
-    run_parallel(NN)
+    run_parallel(NN,numcpu)
