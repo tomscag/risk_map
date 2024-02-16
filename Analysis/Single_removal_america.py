b2a2f7df481aa75764f7d371b4ae2b46227ffc64
Tomas Scagliarini <tomscag92@gmail.com>
2024-02-16 12:00:17 +0100

    (Single_removal_america.py) Parallelized code

--- Analysis/Single_removal_america.py
+++ Analysis/Single_removal_america.py
@@ -23,29 +23,34 @@ def read_network():
     return G0, Gcc[0]
 
 
-def analyze(r0,r1_list,filepath_output):
+def analyze(r0,r1_list):
     """ Simulate the reaction-diffusion over the graph G 
         and save the results in a text file
     """
-    FNAME_OUTPUT = filepath_output + f"{name_topology}_r0_{r0:.5f}.dat"
-    with open(FNAME_OUTPUT,"w+") as file:
-        file.write(f"topology: {name_topology}\nnumber of nodes: {NUM_NODES}\nnumber of samples: {NUM_SAMPLES}\nmax time step: {MAX_TSTEP}\ninitial fraction disrupted nodes: {init0}\n")
-
+    size = 20
     for r1 in r1_list:
-        print(f"analyzing R0: {r0:2.3f}, R1: {r1:2.3f}")
-        O = gillespie_optimized([G,r0,r1,MAX_TSTEP,NUM_SAMPLES,init0])
-        with open(FNAME_OUTPUT,"a+") as file:
-            # file.write(str(r1) + "\t" + str(O) + "\n")
-            file.write(f"{r1:.6f}"+"\t"+f"{O:.6f}"+"\n") 
-        if O < 1e-2: # 
-            break   
+        dict_comp = Counter({})
+
+        for run in range(num_run):
+            nodelist = np.random.choice(range(N),size=size)
+            O,dyn_VS = gillespie_optimized([G,nodelist,r0,r1,MAX_TSTEP,NUM_SAMPLES])
+            G1  = G.subgraph(dyn_VS)
+            cc = sorted(nx.connected_components(G1), key=len, reverse=True)
+            dict_comp += Counter([len(item) for item in cc])
+            print(f"run {run}\t initial node {nodelist}\t GCC {O}")
+
+        file_path = f"OAD_[{r0:3.0f},{r1:3.2f}]_nremovals_{size}_nrun_{num_run}.txt"
+        # Save the dictionary to a text file
+        with open(file_path, 'w') as file:
+            for key, value in dict_comp.items():
+                file.write(f'{key}: {value}\n')
 
 
-def run_parallel(numcpu):
+def run_parallel(r0_list,r1_list,numcpu):
     """ Parallelize the execution of the function analyze """
     pool = mp.Pool(numcpu)
     for i in range(len(r0_list)):
-        pool.apply_async(analyze, args = (r0_list[i], r1_list, filepath_output, ))
+        pool.apply_async(analyze, args = (r0_list[i], r1_list, ))
         # analyze(r0_list[i], r1_list, filepath_output)
     pool.close()
     pool.join()
@@ -58,7 +63,7 @@ def run_parallel(numcpu):
 
 
 if __name__ == "__main__":
-    numcpu = 1
+    numcpu = 3
     num_nodes = 100  # Nodes 
     p_america = 0.00015 
     fpathelist = "./lib/motter/NetworkTopologies/network_america.edgelist"
@@ -71,21 +76,29 @@ if __name__ == "__main__":
     MAX_TSTEP = 10^3
     NUM_SAMPLES = 1 # Samples per node
     
-    r0 = 1e5*1/(N*p_america)
-    r1 = 0e1
-    dict_comp = Counter({})
-    for run in range(num_run):
-        init_node = np.random.choice(range(N),size=10)
-        nodelist = [init_node]
-        O,dyn_VS = gillespie_optimized([G,nodelist,r0,r1,MAX_TSTEP,NUM_SAMPLES])
-        G1  = G.subgraph(dyn_VS)
-        cc = sorted(nx.connected_components(G1), key=len, reverse=True)
-        dict_comp += Counter([len(item) for item in cc])
-        print(f"run {run}\t initial node {init_node}\t GCC {O}")
-    # run_parallel(numcpu)
+    size=20
+    r0 = 0.01*1*(N/size)/(p_america) # Circa 5e4
+    r1 = 1e0
+
+    r0_list = np.arange(5e4,12e4,int(20e3))
+    r1_list = np.arange(0,7,1)
+    run_parallel(r0_list,r1_list,numcpu)
+
+
+    # dict_comp = Counter({})
+    # for run in range(num_run):
+    #     nodelist = np.random.choice(range(N),size=size)
+    #     O,dyn_VS = gillespie_optimized([G,nodelist,r0,r1,MAX_TSTEP,NUM_SAMPLES])
+    #     G1  = G.subgraph(dyn_VS)
+    #     cc = sorted(nx.connected_components(G1), key=len, reverse=True)
+    #     dict_comp += Counter([len(item) for item in cc])
+    #     print(f"run {run}\t initial node {nodelist}\t GCC {O}")
+    
         
-    file_path = 'output_OAD.txt'
-    # Save the dictionary to a text file
-    with open(file_path, 'w') as file:
-        for key, value in dict_comp.items():
-            file.write(f'{key}: {value}\n')
+    # file_path = f"OAD_[{r0:3.0f},{r1:3.2f}]_numremovals_{size}.txt"
+    # # Save the dictionary to a text file
+    # with open(file_path, 'w') as file:
+    #     for key, value in dict_comp.items():
+    #         file.write(f'{key}: {value}\n')
+
+            
